c2e214628981ed6fdbf4c74898d7e201176e1edc
Ayush Jain <ayushjainid@gmail.com>
2022-08-04 13:14:10 -0500

    Updated About Me and Skills

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['C++/C', 'Python', 'SQL', 'JavaScript', 'DevOps Tools', 'Machine Learning'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,31 +135,32 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Ayush. I love building software solutions to solve everyday problems.
+              I also enjoy keeping up new technologies while building deeper Computer Science fundamentals.
+              My interests broadly lies in exploring more about software engineering and machine learning.
+              The fascination which steered me into computer science came from building live object detection
+              models in 2015 continues to inspire me to harness the true potential of computer science for 
+              a better tomorrow.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, I am a Computer Science graduate student at New York University -
+              Courant Institute. I've had the privilege of working as a software developer at {' '}
+              <a href="https://careers.walmart.com/technology">Walmart Global Tech</a>, a Fortune 1
+              company and {' '} <a href="https://amadeus.com/en">Amadeus Labs</a>, a top 10 
+              leading global travel technology company. My main focus these days is to learn building 
+              scalable software solutions with tangile impact utilizing domain and technical knowledge
+              from software engineering, machine learning and blockchain.
             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
+//             <p>
+//               I also recently{' '}
+//               <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
+//                 launched a course
+//               </a>{' '}
+//               that covers everything you need to build a web app with the Spotify API using Node
+//               &amp; React.
+//             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
